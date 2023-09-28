9331180af19567d4e24239645feba3490a944903
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-27 22:31:03 -0300

    first commit

---
+++ src/main/java/com/api/restful/apirestfulspringbootjava/ApiRestfulSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.api.restful.apirestfulspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/restful/apirestfulspringbootjava/ApiRestfulSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.restful.apirestfulspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
