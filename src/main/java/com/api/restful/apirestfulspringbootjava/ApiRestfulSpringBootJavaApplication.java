21e6c1afa244c6e51054e7461094fa10d430aa3c
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-29 07:59:04 -0300

    feat:Alterações

--- src/main/java/com/api/restful/apirestfulspringbootjava/ApiRestfulSpringBootJavaApplication.java
+++ src/main/java/com/api/restful/apirestfulspringbootjava/ApiRestfulSpringBootJavaApplication.java
@@ -1,13 +1,20 @@
 package com.api.restful.apirestfulspringbootjava;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiRestfulSpringBootJavaApplication {
+	private static final Logger logger = LoggerFactory.getLogger(ApiRestfulSpringBootJavaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringBootJavaApplication.class, args);
+		System.out.print("Olá,mundo");
 	}
+
+	public void doSomething() {
+        logger.error("Isso é uma mensagem de erro.");
+    }
 
 }
