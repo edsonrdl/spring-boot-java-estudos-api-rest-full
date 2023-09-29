67b0fbce5010a161abcabc690a3d899275ee4783
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-29 17:04:45 -0300

    feat: Criado TaskController e adicionado list Task

--- src/main/java/com/api/restful/apirestfulspringbootjava/services/TaskService.java
+++ src/main/java/com/api/restful/apirestfulspringbootjava/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.api.restful.apirestfulspringbootjava.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,11 @@ public class TaskService {
         return task.orElseThrow(()-> new RuntimeException(
             "Tarefa não encontrada! id:"+ id + ",Tipos :" + Task.class.getName()
         ));
+    }
+    
+    public List<Task> findAllByUserId(Long userId){
+        List<Task> tasks=this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
